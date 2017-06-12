2a607de2c34010afdff6735b7334dbd8c7846640
Sam Kupfer <sam.kupfer@hobsons.com>
2017-06-12 11:29:21 -0400

    _NEW_SKLEARN_EGG file fix

--- docker_code_build_upload/upload_egg_to_s3.sh
+++ docker_code_build_upload/upload_egg_to_s3.sh
@@ -4,11 +4,7 @@
 
 mv dist/*.egg sklearn.egg
 
-aws s3 sync . s3://hobsons-datascience/retention_modeling/eggs/ --exclude "*" --include "sklearn.egg" | cat -v > sync_output.txt
-
-cat sync_output.txt
-
-if [ $(cat sync_output.txt | grep '\^Mupload: sklearn.egg' | wc -l) -gt 0 ]
+if [ $(aws s3 sync . s3://hobsons-datascience/retention_modeling/eggs/ --exclude "*" --include "sklearn.egg" | cat -v | grep '\^Mupload: \./sklearn\.egg' | wc -l) -gt 0 ]
 then
     touch _NEW_SKLEARN_EGG
     aws s3 cp _NEW_SKLEARN_EGG s3://hobsons-datascience/retention_modeling/eggs/

0e5b71f57a39744177f94e0e5a296af0d009410f
Sam Kupfer <sam.kupfer@hobsons.com>
2017-06-12 10:30:53 -0400

    Added Dockerfile and shell script for Hesos auto-code building

---
+++ docker_code_build_upload/upload_egg_to_s3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script will be run by a Hesos docker container
+
+mv dist/*.egg sklearn.egg
+
+if [ $(aws s3 sync . s3://hobsons-datascience/retention_modeling/eggs/ --exclude "*" --include "sklearn.egg" | cat -v | grep '\^Mupload: sklearn.egg' | wc -l) -gt 0 ]
+then
+    touch _NEW_SKLEARN_EGG
+    aws s3 cp _NEW_SKLEARN_EGG s3://hobsons-datascience/retention_modeling/eggs/
+fi
